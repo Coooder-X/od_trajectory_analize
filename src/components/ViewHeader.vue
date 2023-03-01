2fad351afdef2e92c03c2105c2ccaee8b3ea14f3
Coooder-X <1104143015@qq.com>
2023-03-02 01:18:48 +0800

    feat: 添加 header 组件

---
+++ src/components/ViewHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header">
+    <div class="circle"> {{ viewId }} </div>
+    <span class="title"> {{ title }} </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ViewHeader',
+  components: {},
+   props: {
+    viewId: String,
+    title: String,
+  },
+});
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 35px;
+  background-color: silver;
+}
+
+.circle {
+  position: absolute;
+  margin: 6px 10px 6px 10px;
+  width: 23px;
+  height: 23px;
+  line-height: 23px;
+  background-color: #2c3e50;
+  border-radius: 50%;
+  color: white;
+  text-align: center;
+  font-size: 18px;
+}
+
+.title {
+  margin-left: 40px;
+  font-weight: bolder;
+  font-size: 20px;
+  line-height: 35px;
+}
+
+</style>
--- src/components/views/MapView.vue
+++ src/components/views/MapView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="map-view">
+    <view-header viewId="A" title="地图视图"></view-header>
     <map-comp></map-comp>
   </div>
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import ViewHeader from '../ViewHeader'
 import MapComp from '../MapComp'
-export default {
-  components: { MapComp },
+
+export default defineComponent({
+  components: { MapComp, ViewHeader },
   name: 'MapView',
   props: {
     
   }
-}
+})
 </script>
 
 <style scoped>
