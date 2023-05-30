773a7e916ebce85461ae334a558385dafc82af40
Coooder-X <1104143015@qq.com>
2023-05-30 14:42:23 +0800

    style(前端): 录视频UI修改-视图title & 全局信息 & POI部分约简 & 无用标签删除

--- frontend/src/components/views/MapView.vue
+++ frontend/src/components/views/MapView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map-view">
-    <view-header viewId="B" title="地图视图"></view-header>
+    <view-header viewId="B" title="GIS视图"></view-header>
     <div class="map-view-content">
       <div class="side-bar">
         <div class="side-bar-button" @click="toggleClusterLayer($event)">
@@ -40,7 +40,7 @@
             <img src="@/assets/框选.svg" alt="" />
           </el-tooltip>
         </div>
-        <div class="side-bar-button">
+        <!-- <div class="side-bar-button">
           <el-tooltip
             content="点选"
             placement="right">
@@ -59,7 +59,7 @@
             placement="right">
             <img src="@/assets/路径分析.svg" alt="" />
           </el-tooltip>
-        </div>
+        </div> -->
         <div class="side-bar-button">
           <el-tooltip
             content="过滤"
